556cf52f5fa7255a667b3099eb90642ad62474e8
John S. Hawker <JohnS.Hawker@gmail.com>
2022-10-29 14:55:23 -0600

    Trying chapter 1 programs
    Working on Exercises 1-1 and 1-2.

---
+++ Think Python/ex1-1.py
@@ -0,0 +1,35 @@
+"""
+This exercise covers the print statement and how to print in the terminal.
+
+"""
+#normal print statement
+print("Hello World!")
+
+# print statement with a parentheses missing
+# print("Hello World!"
+# invalid syntax.
+
+# print statement with both parentheses missing
+# print"Hello World!"
+# invalid syntax.
+
+# print statement with a quotation mark missing
+# print("Hello World!)
+# invalid syntax.
+
+# print statement with both quoatation marks missing
+# print(Hello World!)
+# invalid syntax.
+
+# print a digit with a leading zero
+# print(02 + 03)
+# leading zeros will fault out because they are not permitted
+
+x = 2++2
+
+print(x)
+
+# y = 5 7
+# invalid syntax, must have operator between values
+# print(y)
+
---
+++ Think Python/ex1-2.py
@@ -0,0 +1,19 @@
+"""
+For exercise 2 we will use python as a calculator to calculate:
+1. How many seconds in 42 minutes and 42 seconds?
+2. How many miles are there in 10 kilometers?
+3. If you run a 10 kilometer race in 42 minutes and 42 seconds, what is your average pace (time per mile in minutes and seconds)?
+4. What is your average speed in miles per hour?
+"""
+minutes = int(input("Minutes > "))
+seconds = int(input("Seconds > "))
+kilometers = int(input("Kilometers > "))
+
+mintosec = minutes * 60
+totalseconds = mintosec + seconds
+kilotomile = kilometers / 1.609344
+secpermile = kilotomile / totalseconds
+
+
+print(f"There are {totalseconds} in {minutes} minutes and {seconds} seconds.")
+print(f"There are {kilotomile} miles in {kilometers} kilometers")
