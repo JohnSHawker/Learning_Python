01d683805452b5815ec53e8ed7c4f72c5b462795
John S. Hawker <JohnS.Hawker@gmail.com>
2022-10-31 20:06:56 -0600

    Update ex1-2.py
    Finished ex1-2.py

--- Think Python/ex1-2.py
+++ Think Python/ex1-2.py
@@ -5,15 +5,32 @@ For exercise 2 we will use python as a calculator to calculate:
 3. If you run a 10 kilometer race in 42 minutes and 42 seconds, what is your average pace (time per mile in minutes and seconds)?
 4. What is your average speed in miles per hour?
 """
-minutes = int(input("Minutes > "))
-seconds = int(input("Seconds > "))
-kilometers = int(input("Kilometers > "))
+# starting values
+minutes = 42
+seconds = 42
+kilometers = 10
 
-mintosec = minutes * 60
-totalseconds = mintosec + seconds
-kilotomile = kilometers / 1.609344
-secpermile = kilotomile / totalseconds
+# total seconds in 42 minutes and 42 seconds
+totalseconds = (minutes * 60) + seconds
 
+# miles in 10 km
+miles = kilometers / 1.609344
 
-print(f"There are {totalseconds} in {minutes} minutes and {seconds} seconds.")
-print(f"There are {kilotomile} miles in {kilometers} kilometers")
+# seconds per hour to get percentile of hour
+hour = totalseconds / 3600
+
+# hours per mile for pace
+hpm = hour / miles
+pace = hpm * 3600
+# pace minutes
+paceminutes = pace // 60
+# pace seconds 
+paceseconds = pace % 60
+
+# average speed in miles per hour
+mph = miles / hour
+
+print(f"\nThere are {totalseconds} seconds in {minutes} minutes and {seconds} seconds.\n")
+print(f"{kilometers} kilometers is equal to {round(miles, 2)} miles.\n")
+print(f"Your average pace was {paceminutes} minutes and {round(paceseconds, 0)} seconds per mile.\n")
+print(f"Your average speed was {round(mph, 2)} miles per hour.\n")
